cc74e0b0b85ecd4e86cbf85e98600506c3d618a5
clara <clara@clara.com>
2017-02-09 15:56:53 -0600

    more tests

--- Tests/tests_hangman.py
+++ Tests/tests_hangman.py
@@ -20,14 +20,9 @@ class TestGame (unittest.TestCase):
 
 
     def test_game_on(self):
-
         self.assertTrue(hangman.Game_on(5, ['c', '',  't'] , ['c', 'a', 't']))
         self.assertFalse(hangman.Game_on(6, ['c', '',  't'] , ['c', 'a', 't']))
         self.assertFalse(hangman.Game_on(5, ['c', 'a',  't'] , ['c', 'a', 't']))
-
-
-
-
 
     @patch ('builtins.input', side_effect = ['c', 'm', 'v', 'o', 't'])
     @patch ('builtins.print')
@@ -43,10 +38,37 @@ class TestGame (unittest.TestCase):
     def test_right_guess(self, mock_guess):
         self.assertTrue(hangman.user_input())
 
-    @patch('Hangman.hangman.user_input', return_value = 'p')
+
+    @patch('Hangman.hangman.random_choice', side_effect=['coffee'])
+    @patch('builtins.input', side_effect=['c', 'f', 'o', 'e'])
     @patch('builtins.print')
-    def test_main_user_wins(self, mock_print, mock_guess):
-
+    def test_main_user_wins(self, mock_input, mock_random):
         hangman.main()
+        mock_print.assert_any_call('YOU WIN!!')
 
-        mock_print.assert_any_call('Right guess!!!')
+
+
+    @patch('Hangman.hangman.random_choice', side_effect=['cat'])
+    @patch('builtins.input', side_effect=['c', 'a', 't'])
+    @patch('builtins.print')
+    def test_main_user_wins_short_word(self, mock_print, mock_input, mock_random):
+        hangman.main()
+        mock_print.assert_any_call('YOU WIN!!')
+
+
+    @patch('Hangman.hangman.random_choice', side_effect=['binary'])
+    @patch('builtins.input', side_effect=['b', 'i', 'n', 'a', 'r', 'y'])
+    @patch('builtins.print')
+    def test_main_user_wins_short_word(self, mock_print, mock_input, mock_random):
+        hangman.main()
+        mock_print.assert_any_call('YOU WIN!!')
+
+
+
+
+    @patch('Hangman.hangman.random_choice', return_value='coffee')
+    @patch('builtins.input', side_effect=['z', 'f', 'd', 'e', 'g', 'q'])
+    @patch('builtins.print')
+    def test_main_user_loses(self, mock_print, mock_input, mock_random):
+        hangman.main()
+        mock_print.assert_any_call('GAME OVER!')
